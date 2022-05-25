5f5fd73203ef33365638f1ab1f5a39e68d134334
abujakariacse <abujakaria316@gmail.com>
2022-05-25 14:15:02 +0600

    home delivery card

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={carParts} alt="" />
+                <img src={carParts} alt="" className='rounded' />
                 <div>
                     <h1 className="text-5xl font-bold">The World Best Car Parts</h1>
                     <p className="py-6">An car has several numbers of parts. But there are four essential Parts of an car. These are: The Chassis.The Engine.Transmission System.Body.
